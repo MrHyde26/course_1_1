8c0ddf83aa67e871442675434bee240b87b84b0f
a.kravchenko <a.kravchenko@LAPTOP14>
2019-04-06 11:53:06 +0300

    create Calculator

--- course_1_1/Program.cs
+++ course_1_1/Program.cs
@@ -20,7 +20,10 @@ namespace course_1_1
             //OddThreeDigit();
             //
             //
-            XYZ();
+            //XYZ();
+            //
+            //
+            Calculator();
             //
         }
 
@@ -56,7 +59,7 @@ namespace course_1_1
         {
             string strthreedigit = Console.ReadLine();
             int threedigit = Convert.ToInt32(strthreedigit);
-            if (threedigit%2==0 && strthreedigit.Length==3)
+            if (threedigit % 2 == 0 && strthreedigit.Length == 3)
             {
                 Console.WriteLine("True");
             }
@@ -69,7 +72,7 @@ namespace course_1_1
         public static void XYZ()
         {
             int x = 10, y = 12, z = 3;
-            x += y - x++ * 2;
+            x += y - x++ * z;
             Console.WriteLine(x);
             z = --x - y * 5;
             Console.WriteLine(z);
@@ -79,6 +82,47 @@ namespace course_1_1
             Console.WriteLine(z);
             x = y - x++ * 2;
             Console.WriteLine(x);
+        }
+
+        public static void Calculator()
+        {
+            Console.WriteLine("Enter first number");
+            int operand1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int operand2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter arithmetic sign");
+            string sign = (Console.ReadLine());
+            int result;
+            switch (sign)
+            {
+                case "+":
+                    result = operand1 + operand2;
+                    Console.WriteLine($"Result {result}");
+                break;
+                case "-":
+                    result = operand1 - operand2;
+                    Console.WriteLine($"Result {result}");
+                    break;
+                case "*":
+                    result = operand1 * operand2;
+                    Console.WriteLine($"Result {result}");
+                    break;
+                case "/":
+                    if (operand2 != 0)
+                    {
+
+                        result = operand1 / operand2;
+                        Console.WriteLine($"Result {result}");
+                    }
+                    else
+                    {
+                        Console.WriteLine( "Can't divided by zero");
+                    }
+                        break;
+
+                default:
+                    break;
+            }
         }
     }
 }
