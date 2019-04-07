78e283f2a1aaf67789716cd40d6eab3235e28b97
Artem Kravchenko <drta26@gmail.com>
2019-04-07 20:56:48 +0300

    ex3_2

--- course_1_1/Program.cs
+++ course_1_1/Program.cs
@@ -10,10 +10,26 @@ namespace course_1_1
     {
         static void Main(string[] args)
         {
-            
-            
+
+            WorkWithNumber();
+        }
+        public static void WorkWithNumber()
+        {
+            double number = Convert.ToDouble(Console.ReadLine());
+            double counter = 0;
+            for (double i = 1; i <= Math.Abs(number); i++)
+            {
+                if (number % i==0) counter+=1;
+            }
+            if (counter == 2)
+            {
+                Console.WriteLine("You entered a prime number");
+            }
+            else
+            {
+                Console.WriteLine("you");
+            }
         }
 
-       
     }
 }
