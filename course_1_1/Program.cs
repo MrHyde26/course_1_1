73792e7b69d2b5cddff13288c72d74b135a14683
Artem Kravchenko <drta26@gmail.com>
2019-04-08 20:41:18 +0300

    check number

--- course_1_1/Program.cs
+++ course_1_1/Program.cs
@@ -15,20 +15,45 @@ namespace course_1_1
         }
         public static void WorkWithNumber()
         {
-            double number = Convert.ToDouble(Console.ReadLine());
-            double counter = 0;
-            for (double i = 1; i <= Math.Abs(number); i++)
+            int number = Convert.ToInt32(Console.ReadLine());
+            int counter = 0;
+            string result = "";
+            int k;
+            if (number > 0)
+            {
+                result += "Your number is positive";
+            }
+            else if (number < 0)
+            {
+                result += "Your number is negative";
+            }
+            else
+            {
+                result += "Your number is zero";
+            }
+
+            for (int i = 1; i <= Math.Abs(number); i++)
             {
                 if (number % i==0) counter+=1;
             }
             if (counter == 2)
             {
-                Console.WriteLine("You entered a prime number");
+                result += "\nYou entered a prime number";
+                
             }
             else
             {
-                Console.WriteLine("you");
+                result += "\nYou entered not a prime number";
             }
+            int []mas=new int[] { 2, 3, 5, 6, 9 };
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (number % mas[i]==0)
+                {
+                    result += $"\nThe entered number is divisible by {mas[i]} without remainder";
+                }
+            }
+            Console.WriteLine(result);
         }
 
     }
