b370e9a2237b1b5acdcda13c0a04eeb0720ac309
a.kravchenko <a.kravchenko@LAPTOP14>
2019-04-13 11:39:59 +0300

    1. Work With array

--- course_1_1/Program.cs
+++ course_1_1/Program.cs
@@ -10,19 +10,51 @@ namespace course_1_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter a radius");
-            string rstr = Console.ReadLine();
-            int.TryParse(rstr, out int r);
-            Console.WriteLine($"The square of the round {Program.PerformCalculation(r)}");
-            
+            WorkWithMass();
         }
 
-        public static decimal PerformCalculation(int r)
+        public static void WorkWithMass()
         {
-            const decimal Pi = 3.14m;
-            decimal square = Pi * r * r;
-            return square;
-            
+            Console.WriteLine("Enter size of massive");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int []mass = new int [n];
+            int average = 0;
+            int sum = 0;
+            string odd = "Odd numerals: ";
+            Random rand = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                mass[i] = rand.Next(0, 100);
+            }
+
+            int max = mass[0];
+            int min = mass[0];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{mass[i]} ");
+                if (max < mass[i])
+                {
+                    max = mass[i];
+                }
+                if (min > mass[i])
+                {
+                    min = mass[i];
+                }
+                average += mass[i];
+                sum+= mass[i];
+                if (mass[i] % 2 > 0)
+                {
+                    odd += $"{mass[i]} ";
+                }
+            }
+            average = average / (n - 1);
+            Console.WriteLine($"\nMax - {max}");
+            Console.WriteLine($"Min - {min}");
+            Console.WriteLine($"Average - {average}");
+            Console.WriteLine($"Sum - {sum}");
+            Console.WriteLine(odd);
         }
     }
 }
