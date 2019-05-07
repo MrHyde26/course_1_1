e70c7ec99c76247d7f2a542276bc43a5c3bcabf4
a.kravchenko <a.kravchenko@LAPTOP14>
2019-05-07 19:38:45 +0300

    task 2

---
+++ course_1_1/classwork07.05/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace course_1_1.classwork07._05
+{
+    class User
+    {
+        private string Name { get; set; }
+        private int Age { get; set; }
+
+        public User(string name, int age)
+        {
+            Name = name;
+            
+            Age = age;
+        }
+
+    }
+}
---
+++ course_1_1/classwork07.05/Worker2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace course_1_1.classwork07._05
+{
+    class Worker2 : User
+    {
+        private int salary;
+
+        public int Salary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    salary = value;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect Salary ");
+                }
+            }
+        }
+        public Worker2(string name, int age, int salary) : base(name,age)
+        {
+            Salary = salary;
+            
+        }
+    }
+}
